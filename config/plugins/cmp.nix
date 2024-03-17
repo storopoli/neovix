c8681d0d83d581324a407a4e05bff955d8a3a446
Jose Storopoli <jose@storopoli.io>
2024-03-17 11:23:57 -0300

    fix(cmp): update to new configs (#7)

--- config/plugins/cmp.nix
+++ config/plugins/cmp.nix
@@ -5,45 +5,77 @@
     luasnip.config.setup({})
   '';
 
-  plugins.nvim-cmp = {
+  plugins.cmp = {
     enable = true;
 
-    sources = [
-      { name = "copilot"; priority = 100; }
-      { name = "nvim_lsp"; }
-      { name = "luasnip"; }
-      { name = "path"; }
-    ];
+    settings = {
+      sources = [
+        { name = "copilot"; priority = 100; }
+        { name = "nvim_lsp"; }
+        { name = "luasnip"; }
+        { name = "path"; }
+      ];
 
-    mapping = {
-      "<C-Space>" = "cmp.mapping.complete()";
-      "<C-e>" = "cmp.mapping.abort()";
-      "<C-y>" = "cmp.mapping.confirm({ select = true })";
-      "<C-p>" = "cmp.mapping.select_prev_item()";
-      "<C-n>" = "cmp.mapping.select_next_item()";
-      "<C-l>" = {
-        action = ''
-          function()
-            if luasnip.expand_or_locally_jumpable() then
-              luasnip.expand_or_jump()
-            end
-          end
+      mapping = {
+        __raw = ''
+          cmp.mapping.preset.insert({
+            ["<C-Space>"] = cmp.mapping.complete(),
+            ["<C-e>"] = cmp.mapping.abort(),
+            ["<C-n>"] = cmp.mapping.select_next_item(),
+            ["<C-p>"] = cmp.mapping.select_prev_item(),
+            ["<C-y>"] = cmp.mapping.confirm({ select = true }),
+            ["<C-l>"] = cmp.mapping(function()
+              if luasnip.expand_or_locally_jumpable() then
+                luasnip.expand_or_jump()
+              end
+            end, { "i", "s" }),
+            ["<C-h>"] = cmp.mapping(function()
+              if luasnip.locally_jumpable(-1) then
+                luasnip.jump(-1)
+              end
+            end, { "i", "s" }),
+          })
         '';
-        modes = [ "i" "s" ];
       };
-      "<C-h>" = {
-        action = ''
-          function()
-            if luasnip.locally_jumpable(-1) then
-              luasnip.jump(-1)
-            end
-          end
-        '';
-        modes = [ "i" "s" ];
-      };
+
+      snippet.expand = ''
+        function(args)
+        require('luasnip').lsp_expand(args.body)
+        end
+      '';
     };
 
-    snippet.expand = "luasnip";
+
+    cmdline = {
+      "/" = {
+        mapping = { __raw = "cmp.mapping.preset.cmdline()"; };
+        sources = [
+          { name = "buffer"; }
+        ];
+      };
+
+      "?" = {
+        mapping = { __raw = "cmp.mapping.preset.cmdline()"; };
+        sources = [
+          { name = "buffer"; }
+        ];
+      };
+
+      ":" = {
+        mapping = {
+          __raw = ''
+            cmp.mapping.preset.cmdline({
+              ['<S-Tab>'] = cmp.mapping.select_prev_item(),
+              ['<Tab>'] = cmp.mapping.select_next_item(),
+            })
+          '';
+        };
+        sources = [
+          { name = "path"; }
+          { name = "cmdline"; }
+        ];
+      };
+    };
   };
 
   plugins = {
@@ -56,36 +88,6 @@
   };
 
   extraConfigLua = ''
-    local cmp = require('cmp')
-
-    cmp.setup.filetype('gitcommit', {
-      sources = cmp.config.sources({
-        { name = 'git' },
-        { name = 'path' },
-      }, {
-        { name = 'buffer', keyword_length = 3 },
-      })
-    })
-
-    cmp.setup.cmdline({ '/', '?' }, {
-      mapping = cmp.mapping.preset.cmdline(),
-      sources = {
-        { name = 'buffer' }
-      }
-    })
-
-    cmp.setup.cmdline(':', {
-      mapping = cmp.mapping.preset.cmdline({
-        ['<S-Tab>'] = cmp.mapping.select_prev_item(),
-        ['<Tab>'] = cmp.mapping.select_next_item(),
-      }),
-      sources = cmp.config.sources({
-        { name = 'path' }
-      }, {
-        { name = 'cmdline' }
-      })
-    })
-
     require("cmp_git").setup()
 
     local cmp_autopairs = require('nvim-autopairs.completion.cmp')

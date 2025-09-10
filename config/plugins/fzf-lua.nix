29d910bc16fdb07339ac19911d12acefe6131e59
Jose Storopoli <jose@storopoli.com>
2025-09-10 05:19:05 -0300

    fzf: add lsp keymaps

--- config/plugins/fzf-lua.nix
+++ config/plugins/fzf-lua.nix
@@ -8,6 +8,9 @@
       "<C-g>" = "grep";
       "<C-l>" = "live_grep";
       "<C-\\>" = "buffers";
+      "gs" = "lsp_document_symbols";
+      "gS" = "lsp_live_workspace_symbols";
+      "<C-k>" = "diagnostics_workspace";
     };
   };
 }

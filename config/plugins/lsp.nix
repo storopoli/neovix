be3fc030c4bc97e0f171d51351cb2aaa95888036
Jose Storopoli <jose@storopoli.com>
2025-09-12 06:06:30 -0300

    fix: correct packageFallback

--- config/plugins/lsp.nix
+++ config/plugins/lsp.nix
@@ -13,22 +13,28 @@
       "<leader>r" = "rename";
       "<leader>a" = "code_action";
     };
-  };
 
-  lsp.servers = {
-    # Prefer local LSP
-    "*".settings.packageFallback = true;
-
-    lua_ls.enable = true;
-    rust_analyzer.enable = true;
-    hls.enable = true;
-    nil_ls.enable = true;
-    nixd.enable = true;
-    yamlls.enable = true;
-    bashls.enable = true;
-    pyright.enable = true;
-    ruff.enable = true;
-    taplo.enable = true;
+    servers = {
+      lua_ls.enable = true;
+      rust_analyzer = {
+        enable = true;
+        packageFallback = true;
+        installCargo = false;
+        installRustc = false;
+      };
+      hls = {
+        enable = true;
+        packageFallback = true;
+        installGhc = false;
+      };
+      nil_ls.enable = true;
+      nixd.enable = true;
+      yamlls.enable = true;
+      bashls.enable = true;
+      pyright.enable = true;
+      ruff.enable = true;
+      taplo.enable = true;
+    };
   };
 
   # LSP diagnostic configuration

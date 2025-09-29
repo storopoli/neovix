bd6482ce2ed362beaa8f5fca945d0b82acae066f
Jose Storopoli <jose@storopoli.com>
2025-09-29 10:48:14 -0300

    feat(lsp): fidget for lsp

--- config/plugins/lsp.nix
+++ config/plugins/lsp.nix
@@ -4,116 +4,123 @@
 }:
 
 {
-  plugins.lsp = {
-    enable = true;
-    keymaps.lspBuf = {
-      "gd" = "definition";
-      "gD" = "implementation";
-      "gr" = "references";
-      "gi" = "type_definition";
-      "K" = "hover";
-      "<leader>f" = "format";
-      "<leader>r" = "rename";
-      "<leader>a" = "code_action";
-    };
+  plugins = {
+    lsp = {
+      enable = true;
+      keymaps.lspBuf = {
+        "gd" = "definition";
+        "gD" = "implementation";
+        "gr" = "references";
+        "gi" = "type_definition";
+        "K" = "hover";
+        "<leader>f" = "format";
+        "<leader>r" = "rename";
+        "<leader>a" = "code_action";
+      };
 
-    servers = {
-      lua_ls = {
-        enable = true;
-        settings = {
-          Lua = {
-            runtime.version = "LuaJIT";
-            diagnostics.globals = [
-              "vim"
-              "require"
-            ];
-            workspace = {
-              library = lib.nixvim.mkRaw "vim.api.nvim_get_runtime_file('', true)";
-              ignoreDir = [
-                ".git"
-                "node_modules"
-                "target"
-                "deps"
-                ".cache"
-                ".vscode"
-                ".direnv"
-                "result"
+      servers = {
+        lua_ls = {
+          enable = true;
+          settings = {
+            Lua = {
+              runtime.version = "LuaJIT";
+              diagnostics.globals = [
+                "vim"
+                "require"
               ];
+              workspace = {
+                library = lib.nixvim.mkRaw "vim.api.nvim_get_runtime_file('', true)";
+                ignoreDir = [
+                  ".git"
+                  "node_modules"
+                  "target"
+                  "deps"
+                  ".cache"
+                  ".vscode"
+                  ".direnv"
+                  "result"
+                ];
+              };
+              format = {
+                enable = true;
+                defaultConfig = {
+                  indent_style = "space";
+                  indent_size = "2";
+                };
+              };
+              telemetry.enable = false;
             };
-            format = {
-              enable = true;
-              defaultConfig = {
-                indent_style = "space";
-                indent_size = "2";
+          };
+        };
+        rust_analyzer = {
+          enable = true;
+          packageFallback = true;
+          installCargo = false;
+          installRustc = false;
+          settings = {
+            rust-analyzer = {
+              cargo.features = "all";
+              procMacro = {
+                enable = true;
+                ignored = {
+                  napi-derive = [ "napi" ];
+                  async-recursion = [ "async_recursion" ];
+                  async-trait = [ "async-trait" ];
+                };
               };
             };
-            telemetry.enable = false;
           };
         };
-      };
-      rust_analyzer = {
-        enable = true;
-        packageFallback = true;
-        installCargo = false;
-        installRustc = false;
-        settings = {
-          rust-analyzer = {
-            cargo.features = "all";
-            procMacro = {
-              enable = true;
-              ignored = {
-                napi-derive = [ "napi" ];
-                async-recursion = [ "async_recursion" ];
-                async-trait = [ "async-trait" ];
+        hls = {
+          enable = true;
+          packageFallback = true;
+          installGhc = false;
+          settings = {
+            haskell = {
+              formattingProvider = "fourmolu";
+              cabalFormattingProvider = "cabal-fmt";
+              plugin = {
+                fourmolu.config.external = true;
+                rename.config.crossModule = true;
               };
             };
           };
         };
-      };
-      hls = {
-        enable = true;
-        packageFallback = true;
-        installGhc = false;
-        settings = {
-          haskell = {
-            formattingProvider = "fourmolu";
-            cabalFormattingProvider = "cabal-fmt";
-            plugin = {
-              fourmolu.config.external = true;
-              rename.config.crossModule = true;
+        nil_ls = {
+          enable = true;
+          settings = {
+            nil = {
+              formatting.command = [ "nixfmt" ];
             };
           };
         };
-      };
-      nil_ls = {
-        enable = true;
-        settings = {
-          nil = {
-            formatting.command = [ "nixfmt" ];
-          };
-        };
-      };
-      nixd.enable = true;
-      yamlls = {
-        enable = true;
-        settings = {
-          yaml = {
-            format.enable = true;
-            validation = true;
-            schemas = {
-              kubernetes = "*.yaml";
-              "https://raw.githubusercontent.com/compose-spec/compose-spec/master/schema/compose-spec.json" =
-                "docker-compose.yaml";
-              "https://json.schemastore.org/github-workflow.json" = ".github/workflows/*";
-              "https://json.schemastore.org/github-action.json" = ".github/actions/*/action.yaml";
+        nixd.enable = true;
+        yamlls = {
+          enable = true;
+          settings = {
+            yaml = {
+              format.enable = true;
+              validation = true;
+              schemas = {
+                kubernetes = "*.yaml";
+                "https://raw.githubusercontent.com/compose-spec/compose-spec/master/schema/compose-spec.json" =
+                  "docker-compose.yaml";
+                "https://json.schemastore.org/github-workflow.json" = ".github/workflows/*";
+                "https://json.schemastore.org/github-action.json" = ".github/actions/*/action.yaml";
+              };
             };
           };
         };
+        bashls.enable = true;
+        pyright.enable = true;
+        ruff.enable = true;
+        taplo.enable = true;
       };
-      bashls.enable = true;
-      pyright.enable = true;
-      ruff.enable = true;
-      taplo.enable = true;
+    };
+    fidget = {
+      enable = true;
+      # fix transparency issues with some colorschemes
+      settings.notification.window.winblend = 0;
     };
   };
 
